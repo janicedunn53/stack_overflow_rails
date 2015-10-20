f1f9e40d7626d9f908e9c5cb27d03df28056f87c
clinton herrick & janice dunn <student@epicodus.com>
2015-10-20 16:25:14 -0700

    Add factory_girl support.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,7 +4,7 @@ describe Question do
   it { should validate_presence_of :inquiry }
   it { should validate_presence_of :user }
 
-  it 'allows users to input a new question' do
+  it 'allows user to create a new question' do
     question = FactoryGirl.create(:question)
     question.inquiry.should eq "How old is the moon?"
   end
