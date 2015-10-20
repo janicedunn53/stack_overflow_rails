802979c5fdb5225a95f5fdf423ecda1787dc6ad1
clinton herrick & janice dunn <student@epicodus.com>
2015-10-20 12:26:33 -0700

    Update an answer.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -13,6 +13,7 @@ class AnswersController < ApplicationController
       redirect_to question_path(@answer.question)
     else
       render :new
+    end
   end
 
   def show
@@ -22,14 +23,15 @@ class AnswersController < ApplicationController
 
   def edit
     @question = Question.find(params[:question_id])
-    @answer = Answer.find(params[:id])
+    @answer = @question.answers.find(params[:id])
   end
 
   def update
     @question = Question.find(params[:question_id])
-    if @answer.update(@question, answer_params)
-      redirect_to question_path
-    end
+    @answer = @question.answers.find(params[:id])
+    if @answer.update(answer_params)
+      redirect_to question_path(@question)
+    else
       render :edit
     end
   end
