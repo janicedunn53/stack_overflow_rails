95ffca751a01e808a10b3786422e56001fb86b3a
clinton herrick & janice dunn <student@epicodus.com>
2015-10-20 14:41:56 -0700

    Question factory works.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,7 +5,7 @@ describe Question do
   it { should validate_presence_of :user }
 
   it 'allows users to input a new question' do
-    question = FactoryGirl.create(:question, :inquiry => "How old is the moon?", :user => "Dave")
+    question = FactoryGirl.create(:question)
     question.inquiry.should eq "How old is the moon?"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,13 @@
 require 'rails_helper'
 
-  describe User do
-    it { should validate_presence_of :name}
-    it { should validate_presence_of :email}
-    it { should validate_presence_of :password_digest}
+describe User do
+  it { should validate_presence_of :name}
+  it { should validate_presence_of :email}
+  it { should validate_presence_of :password_digest}
 
 
   it "returns user information" do
     user = FactoryGirl.create(:user)
     user.name.should eq "Dave"
   end
-
 end
-
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
